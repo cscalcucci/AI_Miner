65def3d600beec64c4f81e4c22c4855d97f8c3af
cscalcucci <cscal@aphelion-enterprises.com>
2015-10-27 19:47:30 -0400

    Initial Commit

---
+++ Miner.swift
@@ -0,0 +1,13 @@
+//
+//  Miner.swift
+//  AI_Miner
+//
+//  Created by Christopher Scalcucci on 9/10/15.
+//  Copyright (c) 2015 Aphelion. All rights reserved.
+//
+
+import Cocoa
+
+class Miner: NSObject {
+
+}
---
+++ Singleton_Miner.swift
@@ -0,0 +1,9 @@
+//
+//  Singleton_Miner.swift
+//  AI_Miner
+//
+//  Created by Christopher Scalcucci on 10/14/15.
+//  Copyright © 2015 Aphelion. All rights reserved.
+//
+
+import Foundation
