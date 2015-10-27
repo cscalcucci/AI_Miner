b3c25263c6deef34ec703f8de6a46ef349bd7617
cscalcucci <cscal@aphelion-enterprises.com>
2015-10-27 19:48:14 -0400

    Added files to be tracked

--- Miner.swift
+++ Miner.swift
@@ -6,8 +6,100 @@
 //  Copyright (c) 2015 Aphelion. All rights reserved.
 //
 
-import Cocoa
+import Foundation
 
 class Miner: NSObject {
+
+    enum Location : String {
+        case Mine = "Mine"
+        case Bar = "Bar"
+        case Bank = "Bank"
+        case Home = "Home"
+    }
+
+    struct Status {
+        enum Type : String {
+            case Wealth = "Wealth"
+            case Thirst = "Thirst"
+            case Capacity = "Capacity"
+            case Fatigue = "Fatigue"
+        }
+
+        //Status
+        var wealthy : Bool = false
+        var thirsty : Bool = false
+        var tired : Bool = false
+
+        init(type: Type) {
+
+        }
+
+        func checkStatus(type: Type) {
+            switch type {
+                case .Wealth : break
+                case .Thirst : if amount < 5 { validity = false}; break
+                case .Capacity : if amount < 4 { validity = false}; break
+                case .Fatigue : if amount  { validity = false}; break
+            }
+
+            return validity
+        }
+
+
+
+    }
+
+
+
+
+    //Items
+    var ore : Int = 0 {
+        didSet {
+//            updateStatus(Status.Capacity)
+        }
+    }
+    var savings : Int = 0 {
+        didSet {
+//            updateStatus(Status.Wealth)
+        }
+    }
+
+    var drinks : Int = 0
+    var energy : Int = 10
+
+    //Location
+    var location = Location.Home
+
+    //Age
+    var age : Int = 0
+
+    func enterLocation() {
+        switch location {
+            case .Mine : break
+            case .Bar : break
+            case .Bank : break
+            case .Home : break
+        }
+    }
+
+    func updateLocation() {
+        switch location {
+            case .Mine : break
+            case .Bar : break
+            case .Bank : break
+            case .Home : break
+        }
+    }
+
+    func exitLocation() {
+        switch location {
+            case .Mine : break
+            case .Bar : break
+            case .Bank : break
+            case .Home : break
+        }
+    }
+
+
 
 }
--- Singleton_Miner.swift
+++ Singleton_Miner.swift
@@ -7,3 +7,117 @@
 //
 
 import Foundation
+
+class Agent {
+
+}
+
+class State {
+
+}
+
+enum Location : String {
+    case Mine = "Mine"
+    case Bar = "Bar"
+    case Bank = "Bank"
+    case Home = "Home"
+}
+
+enum Item_Type : String {
+    case Gold = "Gold"
+
+}
+
+class Item {
+
+    var type : Item_Type!
+    var amount : Int = 0
+
+    init(type: Item_Type) {
+        self.type = type
+    }
+
+}
+
+
+
+// ==================== Agents ==================== //
+
+class Singleton_Miner : Agent {
+
+    static let sharedInstance = Singleton_Miner()
+
+    var location : Location = Location.Home
+    var name: String!
+
+    var gold = Item(type: Item_Type.Gold)
+
+    var energy : Int = 10
+
+    private override init() {
+        super.init()
+    }
+
+    func changeLocation(newLocation: Location) {
+        location = newLocation
+    }
+
+    func increaseItem(item: Item, amount: Int) {
+        item.amount += amount
+    }
+
+    func decreaseEnergy(amount: Int) {
+        energy -= amount
+    }
+
+    func checkState() {
+        switch self.location {
+            case Location.Mine : break
+            case Location.Bar  : break
+            case Location.Bank : break
+            case Location.Home : break
+        }
+    }
+
+    func 
+
+
+}
+
+// ==================== States ==================== //
+
+
+class Mine : State {
+
+    static let sharedInstance = Mine()
+
+    var miner : Singleton_Miner?
+
+    private override init() {
+        super.init()
+    }
+
+    func enter(miner: Singleton_Miner) {
+
+        if miner.location != Location.Mine {
+          print("\(miner.name): Entering Gold Mine")
+        }
+
+        miner.changeLocation(Location.Mine)
+
+    }
+
+    func execute(miner: Singleton_Miner) {
+
+        let foundAmount = 1
+
+        miner.increaseItem(miner.gold, amount: foundAmount)
+        miner.decreaseEnergy(1)
+
+    }
+
+    func exit(miner: Singleton_Miner) {
+
+    }
+
+}
